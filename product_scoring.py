cb03b3bf9c2be354ea42e0cb9ef19c5e640f2ddc
MazzAhmed5 <40982781+MazzAhmed5@users.noreply.github.com>
2021-10-15 03:12:38 +0500

    removing confidential data

--- product_scoring.py
+++ product_scoring.py
@@ -3,67 +3,9 @@ from apps.catalogue.models import Product
 from apps.partner.utils import ids_to_str
 
 
-BATCH_SIZE = 50000
-
-INSERT_PRODUCT = '''
-INSERT INTO cheetay.catalogue_productstats (product_id, order_rate, normalized_order_rate, favourite_rate,
- normalized_favourite_rate, novelty_rate, normalized_novelty_rate)
-( SELECT 
-    id as product_id, 0.0, 0.0, 0.0 , 0.0, 60.0, 0.0
-     from catalogue_product
-     ) 
-'''
-
-UPDATE_ORDER_RATE = '''
-INSERT INTO cheetay.catalogue_productstats (product_id, order_rate)
-( SELECT 
-    product_id , SUM(quantity) order_rate
-     from 
-     order_line INNER JOIN catalogue_product cp on order_line.product_id = cp.id 
-     group by product_id
-     ) ON DUPLICATE KEY UPDATE order_rate = VALUES(order_rate)
-'''
-
-UPDATE_FAVOURITE_RATE = '''
-INSERT INTO cheetay.catalogue_productstats (product_id, favourite_rate)
-( SELECT 
-    product_id, COUNT(product_id) favourite_rate
-     from 
-     customer_userproductfavourite group by product_id
-     ) ON DUPLICATE KEY UPDATE favourite_rate = VALUES(favourite_rate)
-'''
-
-UPDATE_NOVELTY_RATE = '''
-INSERT INTO cheetay.catalogue_productstats (product_id, novelty_rate)
-( SELECT  P.id as product_id, datediff(CURDATE(), P.date_created) as novelty_rate
- from
-  (select date_created, id
-  from
-   catalogue_product where date_created >= now()-interval 2 month)
-    as P
-    ) ON DUPLICATE KEY UPDATE novelty_rate = VALUES(novelty_rate) 
-'''
-
-PRODUCT_STATS_MIN_MAX = '''
-SELECT 
-    IFNULL(MIN(order_rate), 0), IFNULL(MAX(order_rate), 1),
-    IFNULL(MIN(favourite_rate), 0), IFNULL(MAX(favourite_rate), 1),
-    IFNULL(MIN(novelty_rate), 0), IFNULL(MAX(novelty_rate), 1)
-FROM
-    cheetay.catalogue_productstats
-'''
-
-PRODUCT_NORMALIZE_STATS = '''
-UPDATE cheetay.catalogue_productstats 
-SET 
-    normalized_order_rate = (order_rate - {min_order_rate}) / {order_rate_divisor},
-    normalized_favourite_rate = (favourite_rate - {min_favourite_rate}) / {favourite_rate_divisor},
-    normalized_novelty_rate = ((60 - novelty_rate) - (60 - {max_novelty_rate})) / {novelty_rate_divisor}
-'''
-
 UPDATE_PRODUCT_SCORE = '''
-UPDATE cheetay.catalogue_product p,
-    cheetay.catalogue_productstats as ps
+UPDATE catalogue_product p,
+    catalogue_productstats as ps
 SET 
      p.score = 0.43 * IFNULL(ps.normalized_order_rate, 0) + 0.43 * IFNULL(ps.normalized_favourite_rate, 0) +
      0.14 * IFNULL(ps.normalized_novelty_rate, 0)
@@ -86,7 +28,7 @@ def update_product_score():
     """
 
     with connection.cursor() as cursor:
-        cursor.execute('TRUNCATE TABLE cheetay.catalogue_productstats')
+        cursor.execute('TRUNCATE TABLE catalogue_productstats')
 
     with connection.cursor() as cursor:
         cursor.execute(INSERT_PRODUCT)
